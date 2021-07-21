b342c77ee7f2bd56e2e0f4439dcfe0389839944a
sourceCode-prime <ighotagaddiel98@gmail.com>
2021-07-20 21:44:36 -0400

    added allowed hosts

--- src/voting_system/settings.py
+++ src/voting_system/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = env.get('APP_PRIVATE_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'cuvoting.pythonanywhere.com',
+]
 
 
 # Application definition
